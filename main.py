4842d40c558e20ca4f16b3a8ac64e05c5668330e
Dani <la3.dani.p@gmail.com>
2025-10-28 17:42:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from mean_var_std import calculate
+import mean_var_std
+from unittest import main
 
-if __name__ == '__main__':
-    print(calculate([0,1,2,3,4,5,6,7,8]))
+print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
