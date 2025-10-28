b1530a1f48b7722dd92ab27f43e0c343fc647d99
Dani <la3.dani.p@gmail.com>
2025-10-28 17:44:23 +0330

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,11 +2,11 @@ import numpy as np
 
 # Create a function named calculate() that uses Numpy for the calculation
 # The input of the function should be a list containing 9 digits.
-def calculate(main):
-    if len(main) != 9:
+def calculate(list):
+    if len(list) != 9:
         raise ValueError("List must contain nine numbers.")
     
-    matrix= np.array(main)
+    matrix= np.array(list)
 
 # Convert the list to a 3×3 Numpy array
     matrix = matrix.reshape(3,3)
@@ -18,7 +18,7 @@ def calculate(main):
          func(array).tolist()]
 
 # Calculate the mean, variance, standard deviation, max, min and sum
-    result = {'mean': calc_state(np.mean, matrix),
+    calculations = {'mean': calc_state(np.mean, matrix),
               'variance': calc_state(np.var, matrix),
               'standard deviation': calc_state(np.std, matrix),
               'max': calc_state(np.max, matrix),
@@ -26,4 +26,4 @@ def calculate(main):
               'sum': calc_state(np.sum, matrix),
               }
     
-    return result
+    return calculations
